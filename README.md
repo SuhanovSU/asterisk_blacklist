7226eb3cbada1eaae02388ca50e91b8912ff3753
SuhanovSU <sla-suhanov@yandex.ru>
2020-04-06 16:50:48 +0500

    первое сохранение

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # asterisk_blacklist
+[blcall]
+exten => _XXXX,1,GotoIf($[${BLACKLIST()}]?black)
+exten => s,n,Dial(SIP/${EXTEN},,m)
+exten => s(black),n,Hangup()
---
+++ add_database.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+import MySQLdb
+
+db = MySQLdb.connect(host="rc1c-s8c4zwb4vng2b92n.mdb.yandexcloud.net",  port=3306, user="asterisk", passwd="asterisk",
+                     db="asterisk", ssl={'ca': '~/.mysql/root.crt'})
+
+cursor = db.cursor()
+cursor.execute("select * from blacklist")
+data = cursor.fetchone()
+db.close()
